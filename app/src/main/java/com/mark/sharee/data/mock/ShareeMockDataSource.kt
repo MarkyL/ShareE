3cd0625c362f841161934ab7ef929cb9cae2f94c
Mark Lurie <Mark.lu91@gmail.com>
2020-03-05 21:53:14 +0200

    - Create mock for polls response

--- app/src/main/java/com/mark/sharee/data/mock/ShareeMockDataSource.kt
+++ app/src/main/java/com/mark/sharee/data/mock/ShareeMockDataSource.kt
@@ -18,16 +18,7 @@ class ShareeMockDataSource : ShareeDataSource {
     }
 
     override suspend fun poll(): GeneralPollResponse {
-        val questionsList = listOf(
-            Question( 1, Question.QuestionType.BOOLEAN, "האם אתה מרגיש טוב?"),
-            Question(2, Question.QuestionType.BOOLEAN, "האם קיבלת טיפול בזמן סביר?"),
-            Question(3, Question.QuestionType.NUMERICAL, "מה מידת שביעות הרצון שלך מהטיפול?"),
-            Question(4, Question.QuestionType.NUMERICAL, "מה מידת היחס שקיבלת מהצוות הרפואי והאם תמליץ על שרי?"),
-            Question(5, Question.QuestionType.TEXTUAL, "כיצד היית משפר את השירות במחלקה?"),
-            Question(6, Question.QuestionType.TEXTUAL, "האם תרצה להוסיף עוד משהו?")
-        )
-
-        return GeneralPollResponse("12345", "Mark's mock poll", questionsList.toMutableList())
+        return createGeneralPollResponse1()
     }
 
     override suspend fun submitPoll(verificationToken: String, pollId: String, answeredQuestions: List<AnsweredQuestion>) {
@@ -35,6 +26,63 @@ class ShareeMockDataSource : ShareeDataSource {
     }
 
     override suspend fun getGeneralPolls(verificationToken: String): GeneralPollsResponse {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val pollsList = mutableListOf(
+            createGeneralPollResponse1(), createGeneralPollResponse2(), createGeneralPollResponse3())
+        return GeneralPollsResponse(pollsList)
     }
+
+    //region polls creation mock
+
+    private fun createGeneralPollResponse1(): GeneralPollResponse {
+        val questionsList = listOf(
+            Question(1, Question.QuestionType.BOOLEAN, "האם אתה מרגיש טוב?"),
+            Question(2, Question.QuestionType.BOOLEAN, "האם קיבלת טיפול בזמן סביר?"),
+            Question(3, Question.QuestionType.NUMERICAL, "מה מידת שביעות הרצון שלך מהטיפול?"),
+            Question(
+                4,
+                Question.QuestionType.NUMERICAL,
+                "מה מידת היחס שקיבלת מהצוות הרפואי והאם תמליץ על שרי?"
+            ),
+            Question(5, Question.QuestionType.TEXTUAL, "כיצד היית משפר את השירות במחלקה?"),
+            Question(6, Question.QuestionType.TEXTUAL, "האם תרצה להוסיף עוד משהו?")
+        )
+
+        return GeneralPollResponse("12345", "סקר מוק 1", questionsList.toMutableList())
+    }
+
+    private fun createGeneralPollResponse2(): GeneralPollResponse {
+        val questionsList = listOf(
+            Question(1, Question.QuestionType.BOOLEAN, "האם אתה מרגיש טוב?"),
+            Question(2, Question.QuestionType.BOOLEAN, "האם קיבלת טיפול בזמן סביר?"),
+            Question(3, Question.QuestionType.NUMERICAL, "מה מידת שביעות הרצון שלך מהטיפול?"),
+            Question(
+                4,
+                Question.QuestionType.NUMERICAL,
+                "מה מידת היחס שקיבלת מהצוות הרפואי והאם תמליץ על שרי?"
+            ),
+            Question(5, Question.QuestionType.TEXTUAL, "כיצד היית משפר את השירות במחלקה?"),
+            Question(6, Question.QuestionType.TEXTUAL, "האם תרצה להוסיף עוד משהו?")
+        )
+
+        return GeneralPollResponse("123456", "סקר מוק 2", questionsList.toMutableList())
+    }
+
+    private fun createGeneralPollResponse3(): GeneralPollResponse {
+        val questionsList = listOf(
+            Question(1, Question.QuestionType.BOOLEAN, "האם אתה מרגיש טוב?"),
+            Question(2, Question.QuestionType.BOOLEAN, "האם קיבלת טיפול בזמן סביר?"),
+            Question(3, Question.QuestionType.NUMERICAL, "מה מידת שביעות הרצון שלך מהטיפול?"),
+            Question(
+                4,
+                Question.QuestionType.NUMERICAL,
+                "מה מידת היחס שקיבלת מהצוות הרפואי והאם תמליץ על שרי?"
+            ),
+            Question(5, Question.QuestionType.TEXTUAL, "כיצד היית משפר את השירות במחלקה?"),
+            Question(6, Question.QuestionType.TEXTUAL, "האם תרצה להוסיף עוד משהו?")
+        )
+
+        return GeneralPollResponse("1234567", "סקר מוק 3", questionsList.toMutableList())
+    }
+
+    //endregion
 }
