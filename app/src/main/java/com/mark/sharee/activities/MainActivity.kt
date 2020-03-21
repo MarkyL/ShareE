5720c09e7a0655a646335feb47534e2a5db702c8
Mark Lurie <Mark.lu91@gmail.com>
2020-03-21 21:28:46 +0200

    - Fix for focus losing of app to be handled.

--- app/src/main/java/com/mark/sharee/activities/MainActivity.kt
+++ app/src/main/java/com/mark/sharee/activities/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.mark.sharee.activities
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.example.sharee.R
 import com.mark.sharee.core.ShareeActivity
 import com.mark.sharee.screens.SignInScreen
 import timber.log.Timber
+
 
 class MainActivity : ShareeActivity() {
 
@@ -26,6 +32,23 @@ class MainActivity : ShareeActivity() {
     override fun onPause() {
         super.onPause()
         navigator.dropActivity()
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 
 
