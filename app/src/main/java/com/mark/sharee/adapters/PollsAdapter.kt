880214fae6d354aeafbd85588d616dff8946d05a
Mark Lurie <Mark.lu91@gmail.com>
2020-05-15 16:42:28 +0300

    - Change naming from poll_item to _generic_title_item so it's more generic to be used across app for different adapters.

--- app/src/main/java/com/mark/sharee/adapters/PollsAdapter.kt
+++ app/src/main/java/com/mark/sharee/adapters/PollsAdapter.kt
@@ -5,11 +5,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.sharee.R
 import com.mark.sharee.network.model.responses.GeneralPollResponse
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.poll_item.*
+import kotlinx.android.synthetic.main.generic_title_item.*
 
 class PollsAdapter(listener: AdapterListener<GeneralPollResponse>) : BaseAdapter<GeneralPollResponse>(listener) {
     override fun getLayoutId(position: Int, obj: GeneralPollResponse): Int {
-        return R.layout.poll_item
+        return R.layout.generic_title_item
     }
 
     override fun getViewHolder(view: View, viewType: Int): RecyclerView.ViewHolder {
@@ -21,6 +21,6 @@ class PollViewHolder constructor(override val containerView: View) :
     RecyclerView.ViewHolder(containerView), LayoutContainer, BaseAdapter.Binder<GeneralPollResponse> {
 
     override fun bind(data: GeneralPollResponse) {
-        pollName.text = data.name
+        title.text = data.name
     }
 }
